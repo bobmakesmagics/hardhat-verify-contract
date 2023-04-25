5cf04c3e83c9807d3367f49c1bd0f7a681f14971
bobmakesmagics <Kolyanotmaksim@mail.ru>
2023-04-25 22:16:14 +0300

    update the arguments.js

--- scripts/arguments.js
+++ scripts/arguments.js
@@ -1,10 +1,10 @@
 const ethers = require('ethers')
 module.exports = (deployer_address) => [
-    "Mining License Pool",    // [PROJECT_NAME]
-    "MLP",                    // [PROJECT_SYMBOL]
-    8,                        // [PROJECT_DECIMALS]
-    5000,                     // [PROJECT_SUPPLY]
-    ethers.utils.parseEther('0.02'),  // [INITIAL_LICENSE_PRICE]
-    ["0x5F408676D10e85AcE14342e332f3ffE328f8Ae7d", deployer_address], // [ADMIN_ADDRESS]
-    [25, 75]                                                          // [SHARES OF AMIND AND OWNER]                  
+    "Mining License Pool",                                              // [PROJECT_NAME]
+    "MLP",                                                              // [PROJECT_SYMBOL]
+    8,                                                                  // [PROJECT_DECIMALS]
+    5000,                                                               // [PROJECT_SUPPLY]
+    ethers.utils.parseEther('0.02'),                                    // [INITIAL_LICENSE_PRICE]
+    ["0x5F408676D10e85AcE14342e332f3ffE328f8Ae7d", deployer_address],   // [ADMIN_ADDRESS]
+    [25, 75]                                                            // [SHARES OF ADMIN AND OWNER]                  
 ]
